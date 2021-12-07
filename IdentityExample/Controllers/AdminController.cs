1cf3ab69316b8b7653e9bdbfce6b39d2928acb0d
Mustafa Said Kocatepe <msaidkocatepe@gmail.com>
2021-12-07 23:02:05 +0300

    first migration

---
+++ IdentityExample/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityExample.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace IdentityExample.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager { get; }
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users.ToList());
+        }
+    }
+}
